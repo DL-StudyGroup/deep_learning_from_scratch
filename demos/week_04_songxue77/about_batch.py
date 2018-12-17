37bbcbb3aed8ab9da93541b4df90b560250b1f26
Song <snowbrightly@hotmail.com>
2018-12-17 22:20:04 +0900

    第四周 内容推送1

---
+++ demos/week_04_songxue77/about_batch.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+import sys, os
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록
+import numpy as np
+import pickle
+from dataset.mnist import load_mnist
+from common.functions import sigmoid, softmax
+
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+
+x, _ = get_data()
+network = init_network()
+
+W1, W2, W3 = network['W1'], network['W2'], network['W3']
+
+print(x.shape)
+# (10000, 784)
+print(x[0].shape)
+# (784,)
+print(W1.shape)
+# (784, 50)
+print(W2.shape)
+# (50, 100)
+print(W3.shape)
+# (100, 10)
