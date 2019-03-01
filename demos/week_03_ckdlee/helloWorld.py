799bb6922b3aa8bfd245284b3750e1f70ae85aa1
jiafei427 <jiafei427@gmail.com>
2019-03-01 23:12:42 +0900

    lalalal,longtime no commit..

--- demos/week_03_ckdlee/helloWorld.py
+++ demos/week_03_ckdlee/helloWorld.py
@@ -1,2 +1,20 @@
+import numpy as np
+
 print("hello World...")
 print("hello World...")
+
+x = [[0, 1]]
+# y = [[0], [1], [2]]
+w = [[1, 3, 5], [2, 4, 6]]
+
+x = np.int32(x)
+# y = np.int32(y)
+w = np.int32(w)
+
+print(x)
+print(x.shape)
+# print(y.shape)
+print(w.shape)
+
+print(np.dot(x, w))
+print(np.dot(x, w).shape)
